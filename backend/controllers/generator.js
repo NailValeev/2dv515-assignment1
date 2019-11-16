8532c6885f5baf91c872ae2b83e458a9518808ce
Nail Valeev <nv222ch@student.lnu.se>
2019-11-16 21:00:52 +0100

    Calculation for user similarity (Euklidean) ready

--- backend/controllers/generator.js
+++ backend/controllers/generator.js
@@ -1,58 +1,26 @@
 'use strict';
-
+// import
 let parser = require('./parser');
 
-exports.getAllUsersSimilarity = () => {
-  return (async () => {
-
-    let data = await parser.getUsersData();
-
-    if (!data) return null;
-
-    let result = [];
-
-    for (let i = 0; i < data.length; i++) {
-      let currUser = data[i];
-
-      let infoHolder = new myInfoHolder(currUser.id, currUser.name);
-
-      for (let j = 0; j < data.length; j++) {
-        let compUser = data[j];
-
-        if (currUser.id === compUser.id) {
-          continue;
-        }
-        infoHolder.similarUsers.push(new myResult(compUser.name, compUser.id, getSimilarity(currUser, compUser)));
-      }
-      infoHolder.similarUsers.sort(function (a, b) { return a.score - b.score });
-
-      result.push(infoHolder);
-    }
-    return result;
-
-  })();
-};
-
-
-var myInfoHolder = function (id, name) {
-  this.uid = id + ": " + name;
+// object declaration
+var MyInfoHolder = function (id, name) {
+  this.uid = id + ': ' + name;
   this.similarUsers = []; // TODO sort descend
-};
+}
 
-function myResult(resultName, resultId, score) {
+function MyResult (resultName, resultId, score) {
   this.resultName = resultName;
   this.resultId = resultId;
   this.score = score;
 }
 
-function getSimilarity(userA, userB) {
+// helper function
+function calculateSimilarity (userA, userB) {
   let similarity = 0;
   let matches = 0;
 
   for (let a = 0; a < userA.ratings.length; a++) {
-    console.log("AAAAA compare  for user: " + userA.name)
     for (let b = 0; b < userB.ratings.length; b++) {
-      console.log("FFFFF compare : " + a + "," + userA.ratings[a].movieId + " and " + b + ", " + userB.ratings[b].movieId)
       if (userA.ratings[a].movieId === userB.ratings[b].movieId) {
         matches++;
         similarity += Math.pow((userA.ratings[a].rating - userB.ratings[b].rating), 2);
@@ -65,4 +33,39 @@ function getSimilarity(userA, userB) {
   }
 
   return 1 / (1 + similarity);
+}
+
+/**
+ *  @function
+ *
+ *  @returns Promise with all similarities for users calculated by Euclidean distance
+ */
+exports.getAllUsersSimilarity = () => {
+  return (async () => {
+
+    let data = await parser.getUsersData();
+
+    if (!data) return null;
+
+    let result = [];
+
+    for (let i = 0; i < data.length; i++) {
+      let currUser = data[i];
+
+      let infoHolder = new MyInfoHolder(currUser.id, currUser.name);
+
+      for (let j = 0; j < data.length; j++) {
+        let compUser = data[j];
+
+        if (currUser.id === compUser.id) {
+          continue;
+        }
+        infoHolder.similarUsers.push(new MyResult(compUser.name, compUser.id, calculateSimilarity(currUser, compUser)));
+      }
+      infoHolder.similarUsers.sort(function (a, b) { return b.score - a.score });
+
+      result.push(infoHolder);
+    }
+    return result;
+  })()
 }
--- backend/controllers/parser.js
+++ backend/controllers/parser.js
@@ -1,74 +1,68 @@
 'use strict';
-
+// imports
 const csvToJson = require('convert-csv-to-json');
 
 const users = './datasets/users.csv';
 const ratings = './datasets/ratings.csv';
 const movies = './datasets/movies.csv';
 
-
-var myUser = function (id, name) {
+// object declaration
+var MyUser = function (id, name) {
   this.id = id;
   this.name = name;
   this.ratings = [];
-};
+}
 
-var myRating = function (movieId, rating) {
+var MyRating = function (movieId, rating) {
   this.movieId = movieId;
   this.rating = rating;
-};
+}
 
-var myFilm = function (movieId, year) {
-  this.movieId = movieId;
-  this.year = year;
-};
-
-function parseUsers() {
+// helper functions
+function parseUsers () {
   return (async () => {
     return csvToJson.fieldDelimiter(';').getJsonFromCsv(users);
-  })();
+  })()
 };
 
-function parseMovies() {
+function parseMovies () {
   return (async () => {
     return csvToJson.fieldDelimiter(';').getJsonFromCsv(movies);
-  })();
+  })()
 };
 
-function parseRaitings() {
+function parseRaitings () {
   return (async () => {
     return csvToJson.fieldDelimiter(';').getJsonFromCsv(ratings);
-  })();
+  })()
 };
 
 exports.parseUsers = parseUsers();
 exports.parseMovies = parseMovies();
 exports.parseRaitings = parseRaitings();
 
+/**
+ *  @function
+ *
+ *  @returns Promise with consolidated data arranged by user
+ */
 exports.getUsersData = () => {
   return (async () => {
     let users = await parseUsers();
-    let movies = await parseMovies();
     let ratings = await parseRaitings();
-
-    let films = {};
-    for (let i = 0; i < movies.length; i++) {
-      let title = movies[i].Title;
-      films[title] = new myFilm(movies[i].MovieId, movies[i].Year);
-    }
 
     let persons = [];
     for (let i = 0; i < users.length; i++) {
-      persons[i] = new myUser(users[i].UserId, users[i].Name);
+      persons[i] = new MyUser(users[i].UserId, users[i].Name);
 
       for (let k = 0; k < ratings.length; k++) {
 
         if (persons[i].id === ratings[k].UserId) {
-          persons[i].ratings.push(new myRating(ratings[k].MovieId, ratings[k].Rating))
+          persons[i].ratings.push(new MyRating(ratings[k].MovieId, ratings[k].Rating))
         }
       }
     }
 
     return persons;
-  })();
-};
+  })()
+}
